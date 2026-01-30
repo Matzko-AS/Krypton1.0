61027abec059d3d559c8a9476697478cfbfc43b9
Matzko-AS <msalcivar7626@utm.edu.ec>
2026-01-30 10:34:37 -0500

    primer snapshot

---
+++ src/componentes/form/Form.jsx
@@ -0,0 +1,13 @@
+import "./Form.css"
+
+const form = () => {
+  return (
+    <section className="register">
+       <form action="">
+            <h3>REGISTRO</h3>
+        </form>
+    </section>
+  )
+}
+
+export default form
