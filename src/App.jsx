aa4d6df598fe4e6c65abb948efe63d67dc1ca7bf
Matzko-AS <msalcivar7626@utm.edu.ec>
2026-02-20 11:53:29 -0500

    agregue el fondo y la imagen
    meti el registro e hice que los campos sean
    obligatorios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Form from "./componentes/form/form";
+import Form from "./componentes/form/Form";
 
 function App() {
   const [count, setCount] = useState(0);
